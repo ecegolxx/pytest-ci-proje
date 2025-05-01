a9817863ccf52343ee00c41a2170ace946e68aff
ecegolxx <ecegolx@outlook.com>
2025-05-01 14:56:09 +0300

    Syntax hatası düzeltildi

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-def topla(a,b)
+def topla(a,b):
 	return a+b
-def cikarma(a,b)
+def cikarma(a,b):
 	return a-b
-def carpma(a,b)
+def carpma(a,b):
 	return a*b
-def bolme(a,b)
+def bolme(a,b):
 	return(a/b)
 
